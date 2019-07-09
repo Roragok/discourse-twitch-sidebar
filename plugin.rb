c62f0ecbe7803b96d4c1401470cae95abbc2aa43
PhiL <roragok@gmail.com>
2019-07-09 00:48:44 +0000

    add git link to plugin

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,7 @@
 # about: A twitch sidebar that works with discourse-layouts
 # version: 0.1
 # authors: Roragok
+# url: https://git.roragok.com/namafia/discourse-twitch-sidebar
 
 register_asset 'stylesheets/twitch.scss'
 enabled_site_setting :twitch_sidebar_user
