fa65c1fe442c9115466cafff49fd3a5483d63756
PhiL <hagar_philip@bah.com>
2019-07-10 09:28:12 -0400

    update the plugin info name and add a enabled setting for boolean. hopefully this enables linking to settings from plugin page

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,11 @@
-# name: Twitch_Sidebar
+# name: discourse-twitch-sidebar
 # about: A twitch sidebar that works with discourse-layouts
 # version: 0.1
-# authors: Roragok
+# authors: Roragok, epok
 # url: https://git.roragok.com/namafia/discourse-twitch-sidebar
 
 register_asset 'stylesheets/twitch.scss'
-enabled_site_setting :twitch_sidebar_user
+enabled_site_setting :twitch_sidebar_enabled
 
 DiscourseEvent.on(:layouts_ready) do
   DiscourseLayouts::WidgetHelper.add_widget('twitch')
