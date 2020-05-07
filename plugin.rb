51ea28743552b5984e01c1b024da88f3aa92e251
PhiL <hagar_philip@bah.com>
2020-05-07 10:12:44 -0400

    update how layouts is called since it moved to v2

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-twitch-sidebar
 # about: A twitch sidebar that works with discourse-layouts
-# version: 1.0
+# version: 1.1
 # authors: Roragok, epok
 # url: https://git.roragok.com/namafia/discourse-twitch-sidebar
 
@@ -8,5 +8,5 @@ register_asset 'stylesheets/twitch.scss'
 enabled_site_setting :twitch_sidebar_enabled
 
 DiscourseEvent.on(:layouts_ready) do
-  DiscourseLayouts::WidgetHelper.add_widget('twitch')
+  DiscourseLayouts::Widget.add('twitch')
 end
